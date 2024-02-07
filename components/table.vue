a4b2b201ba6c9a88af1f18267b525cb1f65db280
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2024-02-08 01:12:21 +0800

    refactor table component + add searchable

--- components/table.vue
+++ components/table.vue
@@ -3,6 +3,14 @@
 // ref for loading state
 const loading = ref(true);
 const people = ref([]);
+const page = ref(1)
+const pageCount = 1
+const totalItems = ref(people.value.length)
+const q = ref('');
+
+watch(q, () => {
+  page.value = 1;
+});
 
 const columns = [
   {
@@ -32,31 +40,43 @@ await useLazyAsyncData(async (nuxtApp) => {
     loading.value = false;
   });
 })
-const page = ref(1)
-const pageCount = 10
-const rows = computed(() => {
-  return people.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+
+const filteredRows = computed(() => {
+  if (!q.value) {
+    totalItems.value = people.value.length;
+    return people.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+  }
+  let filtered = people.value.filter((person) => {
+    return Object.values(person).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase())
+    })
+  })
+  totalItems.value = filtered.length
+  return filtered.slice((page.value - 1) * pageCount, (page.value) * pageCount)
 })
 
 </script>
 
 <template>
-    <UTable :ui="{
-        base: 'min-w-full table-fixed rounded-lg',
-        th: {
-            color: 'text-black dark:text-black'
-        },
-        td: {
-            color: 'text-black dark:text-black'
-        },
-        emptyState: {
-            label: 'dark:text-black'
-        }
-    }" :columns="columns" :rows="rows"
-        :empty-state="{ icon: 'i-heroicons-user-20-solid', label: 'No Members Registered/Approved yet.' }"
-        :loading="loading" />
-    <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-        <UPagination v-model="page" :page-count="pageCount" :total="people.length" />
+  <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <UInput v-model="q" placeholder="Filter people..." @change="page.value = 1" />
     </div>
+  <UTable :ui="{
+    base: 'min-w-full table-fixed rounded-lg',
+    th: {
+      color: 'text-black dark:text-black'
+    },
+    td: {
+      color: 'text-black dark:text-black'
+    },
+    emptyState: {
+      label: 'dark:text-black'
+    }
+  }" :columns="columns" :rows="filteredRows"
+    :empty-state="{ icon: 'i-heroicons-user-20-solid', label: 'No Members found.' }"
+    :loading="loading" />
+  <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+    <UPagination v-model="page" :page-count="pageCount" :total="totalItems" />
+  </div>
 </template>
 
